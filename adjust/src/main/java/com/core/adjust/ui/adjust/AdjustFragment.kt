144119f8777534fce839febacfc221a137debbd7
Tuấn Anh <vtabk2@gmail.com>
2025-11-03 22:22:33 +0700

    adjust

--- adjust/src/main/java/com/core/adjust/ui/adjust/AdjustFragment.kt
+++ adjust/src/main/java/com/core/adjust/ui/adjust/AdjustFragment.kt
@@ -22,7 +22,7 @@ class AdjustFragment : Fragment(R.layout.f_fragment_adjust) {
             AdjustSlider("contrast", "Contrast", R.drawable.ic_contrast),
             AdjustSlider("highlights", "Highlights", R.drawable.ic_highlights),
             AdjustSlider("shadows", "Shadows", R.drawable.ic_shadows),
-            AdjustSlider("whites", "Whites", R.drawable.ic_whites),
+            AdjustSlider("whites", "Whites", R.drawable.selector_ic_whites),
             AdjustSlider("blacks", "Blacks", R.drawable.selector_ic_blacks),
 
             AdjustSlider("temperature", "Temperature", R.drawable.ic_temperature),
